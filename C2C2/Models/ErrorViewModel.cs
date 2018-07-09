44ab02ebe09d2aab5cac034cce66241bc2b759b5
Anchal.Mathur <Anchal.Mathur@BOLD-ANCHALM>
2018-07-09 12:43:42 +0530

    Add project files.

---
+++ C2C2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C2C2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ C2C2/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C2C2.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
