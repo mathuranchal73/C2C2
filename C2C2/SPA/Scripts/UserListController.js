9fb876d3b6d452c8a7efafce26ef727d50898921
Anchal.Mathur <Anchal.Mathur@BOLD-ANCHALM>
2018-07-18 12:48:19 +0530

    WebAPI included

---
+++ C2C2/SPA/Scripts/UserListController.js
@@ -0,0 +1,8 @@
+﻿(function (app) {
+    var MusicListController = function ($scope) {
+        $scope.message = "Manish Kumar";
+    };
+    app.controller("UserListController", UserListController);
+
+}
+    (Angular.module("homepage")));  
